640785f3732367fe5b18e7f232e8a60d170089cd
sergisantana <sergi.santana.ruiz@gmail.com>
2015-11-21 16:35:22 +0100

    Modificacion build_database
    Se ha realizado un cambio de algoritmo. La función anterior leía el
    fichero Annotation.txt y generaba un archivo .txt y ahora lee las
    imágenes y guarda el nombre de cada imagen in extensión en un archivo
    txt

--- build_database.py
+++ build_database.py
@@ -1,46 +1,50 @@
 # -*- coding: utf-8 -*-
-import cv2 #Llibreria: http://opencv.org/
-import numpy as np # Llirebria: http://www.numpy.org/
-import matplotlib.pyplot as plt
-from scipy.misc import imsave
-from scipy import misc
-from glob import glob
 
 
-# Lee el fichero (annotation.txt )donde aparecen el nombre de la clase con el nombre del archivo
-# y lo almacena en dos variables. array_id (numero) y array_clase(catedral, etc..). Despues lo escribe en un fichero .txt
+
+##import cv2 #Llibreria: http://opencv.org/
+#import numpy as np # Llirebria: http://www.numpy.org/
+#import matplotlib.pyplot as plt
+import os
+#from scipy.misc import imsave
+#from scipy import misc
+#from glob import glob
+
+
+# Lee las imagenes de una carpeta y almacena el las ID's de cada imagen en un fichero txt
 
 #450 imagenes en la carpeta train
 #180 imagenes en la carpeta val
 
-num_picture_train=450+1;
-num_picture_val=180+1;
+#num_picture_train=450+1;
+#num_picture_val=180+1;
 
-i=0 #variable de iteración para guardar cada palabra
-array_id = ["" for x in range(num_picture_val)] # Vector que almacena las id's aleatorias
-array_clase = ["" for x in range(num_picture_val)] #Vector que almacena el nombre de la case (catedral...etc.)
-fichero_final=open("Text_ID_picture.txt","w") # abre un fichero
-with open('./TerrassaBuildings900/val/annotation.txt','r') as fichero:
-    ##linia= fichero.readlines()
-    for linea in fichero: # mientras haya lineas..
-        words = linea.split() # lee las palabras de cada linia haciendo un split()
-                                # la variable words tiene las posiciones que palabras haya en la linia, en este caso dos
-        array_id[i]=words[0] # almacena la id aleatoria
-        array_clase[i]=words[1] # almaena el nombre de la clase
-        fichero_final.write(words[0]+"\n") #escribe la id en el fichero por cada fichero
-        i=i+1 # incrementa la iteración para que el vector tenga las posiciones que debe tener(180)
+#Para utilizar el patch se debe utilizar una estructura asi:
+
+#         /     /TerrassaBuildings900 -> Aqui estaran las imagenes
+#         /     /src                  -> Aqui guardaremos los archivos .py
+#         /     /txt                  -> Guardaremos los ficheros txt generados
+#         /     /build_database.py
 
 
-fichero_final.close() # cierra el fichero_final
+patch_imagenes_train = "./TerrassaBuildings900/train/images/"
+patch_imagenes_val = "./TerrassaBuildings900/val/images/"
+#dir_src = "./src"
+#dir_actual = "./"
+dir_archivos_txt = "./txt/"
 
 
-#Pruebas para comprobar que la id[1] corresponde al nombre clase[1]
-#Las posiciones 0 quedan utilizadas para almacenar el nombre de los campos ImageID y ClassID
 
-print("Image ID: " + array_id[1])
-print("Class ID: " + array_clase[1])
+def build_database(directorio_imagenes, directorio_txt, trainorval): #Se define una funcion
+    imagenes_dir = os.listdir(directorio_imagenes) #Asigna el nombre de cada imagen a una posición del vector image_files[1]
+    print("Archivos leidos del directorio(patch absoluto): " + os.path.abspath(directorio_imagenes))
+    fichero_txt = open(directorio_txt + 'ID_images_'+trainorval+'.txt', 'w') # Abre el fichero donde guardaremos las ID's
+    for imagenes  in imagenes_dir: #Mientras haya imagenes en el directorio imagenes_dir...
+        fichero_txt.write(imagenes[0:-4] + "\n") #escribe la el nombre de la id sin el jpg y añade un INTRO al final de cada nombre
 
-print(words) # muestra la última posicion la 180 del vector, que es la ultima que ha leido
 
-print("Image ID: " + array_id[180])
-print("Class ID: " + array_clase[180])
+
+# Crear los dos ficheros txt con las ID para cada directorio
+
+build_database(patch_imagenes_train,dir_archivos_txt,"train")
+build_database(patch_imagenes_val,dir_archivos_txt,"val")
