9d969b0e987eca45afdbce0dee61fde290b2e567
Carlos Manso <carlos.manso@gmail.com>
2015-11-22 14:32:07 +0100

    crear el directorio si no esta y arreglo general

--- src/get_features.py
+++ src/get_features.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-
 import cv2
-#import cPickle as pickle
 import pickle #Ejemplos de serialización: https://docs.python.org/2/library/pickle.html
 import numpy as np
-
+import os
 
 #450 imagenes en la carpeta train
 #180 imagenes en la carpeta val
@@ -12,28 +10,21 @@ import numpy as np
 #num_picture_train=450+1;
 #num_picture_val=180+1;
 
-#Para utilizar las variables patch se debe utilizar una estructura asi:
+#    Para utilizar el path se debe utilizar una estructura asi:
 
-#         /     /TerrassaBuildings900             -> Aqui estaran las imagenes
-#         /     /src                              -> Aqui guardaremos los archivos .py
-#
-#         /                 /build_database.py      -> Archivo build_database
-#         /                 /get_features.py        -> Archivo get_features.py
-#         /     /txt                              -> Guardaremos los ficheros txt generados
+#    TerrassaBuildings900               -> Aquí deben estar las imágenes
+#    src                                -> Aquí deben estar los archivos .py
+#    txt                                -> Aquí guardaremos los ficheros txt generados
 
-#         /     /descriptores/train y /val
 
 
 
 
 path_imagenes_train = "../TerrassaBuildings900/train/images/"
 path_imagenes_val = "../TerrassaBuildings900/val/images/"
-#dir_src = "././src"
-#dir_actual = "././"
 dir_archivos_txt = "../txt/"
 dir_descriptores = "../descriptores/"
-dir_descrip_val = "../descriptores/val/"
-dir_descrip_train = "../descriptores/train/"
+
 
 #archivo image id es el nombre del archivo. En este caso hay dos ID_images_train.txt o ID_images_val.txt
 
@@ -41,18 +32,25 @@ def get_features(directorio_imagenes, archivo_image_id, directorio_descriptores)
 
 
     archivo_imageid = open(dir_archivos_txt+archivo_image_id, 'r') #Abrir el archivo de con las ID's de las imangenes
-    #descriptor_file = open(directorio_descriptores+"Descriptors.txt", 'w') #Abrir un fichero txt para guardar los descriptores
     dict_caracteristicas={} #creamos un diccionario vacio
+    
+    if not os.path.exists(directorio_descriptores):
+        os.makedirs(directorio_descriptores)
+
+
     for id_linia in archivo_imageid:
 
         vector_caract_random = np.random.random_integers(0, 255, 100) ;#Te genera un array con  100 numeros random entre 0 y 255
         linia = id_linia.split(); # lee la linia del fichero
         dict_caracteristicas [linia[0]] = vector_caract_random #asignamos por cada linia un vector de caracteristicas diferente
 
+
         if archivo_image_id == "ID_images_train.txt":
-            pickle.dump(dict_caracteristicas, open(dir_descrip_train+"descriptor_" + "train.p", "wb" ) ) # guarda el diccionario en un archivo .p
+            pickle.dump(dict_caracteristicas, open(directorio_descriptores+"descriptor_" + "train.p", "wb" ) ) # guarda el diccionario en un archivo .p
         else:
-            pickle.dump(dict_caracteristicas, open(dir_descrip_val+"descriptor_" + "val.p", "wb" ) ) # guarda el diccionario en un archivo .p
+            pickle.dump(dict_caracteristicas, open(directorio_descriptores+"descriptor_" + "val.p", "wb" ) ) # guarda el diccionario en un archivo .p
+
+
 
     archivo_imageid.close()
     #descriptor_file.close()
@@ -63,5 +61,5 @@ get_features(path_imagenes_val, "ID_images_val.txt", dir_descriptores)
 
 #Load a pickle file
 
-dict_train = pickle.load( open(dir_descrip_val+"descriptor_val.p", "rb" ) ) #lee el archivo creado .p donde esta el diccionario.
-dict_train = pickle.load( open(dir_descrip_train+"descriptor_train.p", "rb" ) )#
+dict_val = pickle.load( open(dir_descriptores+"descriptor_val.p", "rb" ) ) #lee el archivo creado .p donde esta el diccionario.
+dict_train = pickle.load( open(dir_descriptores+"descriptor_train.p", "rb" ) )#
