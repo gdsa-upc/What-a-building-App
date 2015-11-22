2e80c704726492ccbd9d3770e87f22379c2fcb4a
Carlos Manso <carlos.manso@gmail.com>
2015-11-22 14:13:49 +0100

    crear el directorio si no esta y quitar comentarios de las librerias.

--- src/build_database.py
+++ src/build_database.py
@@ -1,51 +1,39 @@
 # -*- coding: utf-8 -*-
 
-
-##import cv2 #Llibreria: http://opencv.org/
-#import numpy as np # Llirebria: http://www.numpy.org/
-#import matplotlib.pyplot as plt
 import os
-#from scipy.misc import imsave
-#from scipy import misc
-#from glob import glob
 
 
-# Lee las imagenes de una carpeta y almacena el las ID's de cada imagen en un fichero txt
-
-#450 imagenes en la carpeta train
-#180 imagenes en la carpeta val
-
-#num_picture_train=450+1;
-#num_picture_val=180+1;
-
-#Para utilizar el patch se debe utilizar una estructura asi:
-
-#         /     /TerrassaBuildings900         -> Aqui estaran las imagenes
-#         /     /src   build_database.py      -> Aqui guardaremos los archivos .py
-#         /     /txt                          -> Guardaremos los ficheros txt generados
-#         /     /
+#    Lee las imagenes de una carpeta y almacena el las ID's de cada imagen en un fichero txt
 
 
-#Para utilizar las variables de patch debemos ejecutar el script desde la carpeta /src.
+#    Para utilizar el path se debe utilizar una estructura asi:
+
+#    TerrassaBuildings900               -> Aquí deben estar las imágenes
+#    src                                -> Aquí deben estar los archivos .py
+#    txt                                -> Aquí guardaremos los ficheros txt generados
+
+
+#    Para utilizar las variables de path debemos ejecutar el script desde la carpeta /src.
 
 path_imagenes_train = "../TerrassaBuildings900/train/images/" #Esta variable la utilizamos para referirnos al directorio donde extraer las imagenes.
 path_imagenes_val = "../TerrassaBuildings900/val/images/" #Si estamos en el directorio /src y ejecutamos ../ volvemos al directorio el cual pertenece que es nuestro directorio master.
-#dir_src = "../src"                     #
-#dir_actual = "./"
 dir_archivos_txt = "../txt/"
 
 
-
-def build_database(directorio_imagenes, directorio_txt, trainorval): #Se define una funcion
+def build_database(directorio_imagenes, directorio_txt, trainorval):
     imagenes_dir = os.listdir(directorio_imagenes) #Asigna el nombre de cada imagen a una posición del vector image_files[1]
-    print("Archivos leidos del directorio(patch absoluto): " + os.path.abspath(directorio_imagenes))
+    print("Archivos leidos del directorio(path absoluto): " + os.path.abspath(directorio_imagenes))
+    if not os.path.exists(dir_archivos_txt):
+        os.makedirs(dir_archivos_txt)
+
     fichero_txt = open(directorio_txt + 'ID_images_'+trainorval+'.txt', 'w') # Abre el fichero donde guardaremos las ID's
-    for imagenes  in imagenes_dir: #Mientras haya imagenes en el directorio imagenes_dir...
-        fichero_txt.write(imagenes[0:-4] + "\n") #escribe la el nombre de la id sin el jpg y añade un INTRO al final de cada nombre
+    for imagenes  in imagenes_dir: # Mientras haya imagenes en el directorio imagenes_dir...
+        fichero_txt.write(imagenes[0:-4] + "\n") # escribe la el nombre de la id sin el jpg y añade un INTRO al final de cada nombre
+
 
 
 
 # Crear los dos ficheros txt con las ID para cada directorio
 
-build_database(path_imagenes_train,dir_archivos_txt,"train")
-build_database(path_imagenes_val,dir_archivos_txt,"val")
+build_database(path_imagenes_train, dir_archivos_txt, 'train')
+build_database(path_imagenes_val, dir_archivos_txt, 'val')
