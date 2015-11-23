7b7930bc0fe0ce51cd29c50dd427fe41846d7b59
Carlos Manso <carlos.manso@gmail.com>
2015-11-23 13:38:09 +0100

    Update rank.py

--- src/rank.py
+++ src/rank.py
@@ -7,7 +7,7 @@ import numpy as np
 def distance(x, y): 
     return np.random.randint(0, 451)
 
-def rank(features_file, results_dir, features_train):
+def rank(features_file, results_dir, features_train, annotation_path):
     features_val = pickle.load( open("../descriptores/"+features_file, "r" ) )
     features_train = pickle.load( open("../descriptores/"+features_train, "r" ) )
     
@@ -19,16 +19,17 @@ def rank(features_file, results_dir, features_train):
     # Por cada imagen en el set de validación, creamos un ranking de las imagenes de training y lo escribimos en fichero
     vector= []
     for k_val, v_val in features_val.items():
-        rank= open(results_dir + "/rank_" + k_val +'.txt', 'w')
-        for k_train, v_train in features_train.items():
-            # Como aún no debe estar implementado del todo, calculamos la "distancia" solo con el primer valor
-            vector.insert( distance(v_train[0], v_val[0])  ,k_train) 
-        for item in vector:
-            rank.write("%s\n" % item)
-        vector=[]
-        rank.close()
-    
-    
+        annotations= open(annotation_path, "r")
+        for line in annotations:
+            rec= line.split("\t")
+            if rec[0]==k_val and rec[1]!="desconegut\n":
+                rank= open(results_dir + "/rank_" + k_val +'.txt', 'w')
+                for k_train, v_train in features_train.items():
+                    vector.insert( distance(v_train[0], v_val[0]), k_train) 
+                for item in vector:
+                    rank.write("%s\n" % item)
+                vector=[]
+                rank.close()
 
 # Ejecutamos
-rank("descriptor_val.p", "../rank/", "descriptor_train.p")
+rank("descriptor_val.p", "../rank/", "descriptor_train.p", "../TerrassaBuildings900/val/annotation.txt")
