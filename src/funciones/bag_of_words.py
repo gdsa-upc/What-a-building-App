c327c19a2676d69aee9830e44fec768dee40800d
Carlos Manso <carlos.manso@gmail.com>
2015-11-28 12:45:53 +0100

    Update bag_of_words.py

--- src/funciones/bag_of_words.py
+++ src/funciones/bag_of_words.py
@@ -1,24 +1,26 @@
+# -*- coding: utf-8 -*-
 from funciones.get_assignments import get_assignments
 import matplotlib.pyplot as plt
 from funciones.train_codebook import train_codebook
 from funciones.get_local_features import get_local_features
 from scipy.cluster.vq import vq, kmeans, whiten
-import collections
+import numpy as np
 from sklearn import preprocessing
+import collections
 
 def bag_of_words(assignments):
-    counter= collections.Counter(assignments)
-    counter_v= counter.values()
-    counter_nor= preprocessing.normalize(counter_v)
-    #se tiene que devolver counter_nor, pero para pruebas es mejor verlo con counter_v
+    counter_v= np.bincount(assignments)
+    
+    #counter= collections.Counter(assignments)
+    #counter_nor= preprocessing.normalize(counter_v)
+    #se tiene que devolver counter_nor, pero para pruebas es mejor verlo con counter_v y además suelta warnings
     return counter_v
-
 
 
 """
 descriptoresss = whiten(get_local_features("../TerrassaBuildings900/train/images/4406-18633-1754.jpg"))
 
-codebook1 = train_codebook(5, descriptoresss)
+codebook1 = train_codebook(10, descriptoresss)
 descriptores2= whiten(get_local_features("../TerrassaBuildings900/val/images/22053-9694-8921.jpg"))
 assig = get_assignments(codebook1, descriptores2)
 
