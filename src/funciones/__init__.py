49b372d3f1cde15741bd2acd7ce240fada866a2f
Carlos Manso <carlos.manso@gmail.com>
2015-11-29 02:52:08 +0100

    Update __init__.py

--- src/funciones/__init__.py
+++ src/funciones/__init__.py
@@ -1,1 +1,7 @@
 
+from get_local_features import get_local_features
+from train_codebook import train_codebook
+from get_assignments import get_assignments
+from build_bow import build_bow
+
+__all__= ["get_local_features", "train_codebook", "get_assignments", "build_bow"]
