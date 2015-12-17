34461381206e1fbb09d833d08f6ad81f6f0e129a
AlbertAltarriba <albertaltfer@gmail.com>
2015-12-17 13:30:55 +0100

    Plot confusion matrix

--- src/evaluate_classification.py
+++ src/evaluate_classification.py
@@ -22,6 +22,24 @@ path_imagenes_val = "../TerrassaBuildings900/val/images/"
 dir_archivos_txt = "../txt/"
 dir_descriptores = "../descriptores/"
 
+def plot_confusion_matrix(cm, true_labels,normalize = False,title='Confusion matrix', cmap=plt.cm.Blues):
+    
+    # Normalize matrix rows to sum 1
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
+    plt.imshow(cm, interpolation='nearest',cmap=cmap)
+    
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(true_labels))
+    plt.xticks(tick_marks, true_labels, rotation=90)
+    plt.yticks(tick_marks, true_labels)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+    plt.show()
 
 f = open("../TerrassaBuildings900/val/annotation.txt", "r") #Obrim l'arxiu per llegir el validation de ground truth
 gt_val = f.readlines() #creem una llista amb cada un dels elements del .txt
@@ -38,7 +56,7 @@ gt_val_ID.pop(0)
 for i in gt_val:
     label = i.replace('\n','') #eliminem els \n
     label=label.split('\t') #separem les linies en dos a partir del \t
-    gt_val_ID[j]=label[0]
+    gt_val_ID[j]=label[0]#agafem només el nom de la id
     label=label[1] #agafem només el nom de la classe
     gt_val[j]=label #guardem al vector només el nom de l'etiqueta
     j=j+1
@@ -62,24 +80,24 @@ for i in aa_val:
     j=j+1
      
 j=0
-prova=[]
-prova_ID=[]
+aa_val_aux=[]
+aa_val_ID_aux=[]
 for i in aa_val:
-    w = gt_val_ID[j]
+    id_image = gt_val_ID[j]
     k=0
     for l in aa_val_ID:
-        if l==w:
-            prova.append(aa_val[k])
-            prova_ID.append(aa_val_ID[k])
+        if l==id_image:
+            aa_val_aux.append(aa_val[k])
+            aa_val_ID_aux.append(aa_val_ID[k])
         k=k+1     
     j=j+1
 
-aa_val=prova
-aa_val_ID=prova_ID
+aa_val=aa_val_aux
+aa_val_ID=aa_val_ID_aux
 
 # accuracy test
-#y_true = [gt_train]
-#y_pred = [aa_train]
+#y_true = [gt_val]
+#y_pred = [aa_val]
 
 print("Accuracy")
 print( accuracy_score(gt_val, aa_val))
@@ -94,7 +112,6 @@ print("\n")
 print("Precision per cada classe")
 print(precision_score(gt_val, aa_val, average=None)) #ho calcula per cada classe
 print("\n")
-
 
 #recall test
 # recall is the ratio tp / (tp + fn) 
@@ -120,3 +137,8 @@ print("\n")
 #confusion matrix test
 print("Matriu confusió")
 print(confusion_matrix(gt_val, aa_val))
+
+cm = confusion_matrix(gt_val,aa_val)
+classes = np.unique(gt_val) #llista de les classes
+plt.figure(1)
+plot_confusion_matrix(cm,classes,normalize=True)
